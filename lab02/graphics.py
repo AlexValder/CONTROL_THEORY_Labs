7bb4a5a01b5230af21d59f90baa75d0c3f01b942
AlexValder <angelaarhangelska@gmail.com>
2021-03-17 00:07:00 +0200

    kmp

--- lab02/graphics.py
+++ lab02/graphics.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+from typing import List
 
 
-def draw_phase_trajectory() -> None:
+def draw_phase_trajectory(xs: List[float], ys: List[float]) -> None:
     plt.xlabel('θ')
     plt.ylabel('dθ/dx')
     plt.title('Phase space trajectory')
--- lab02/main.py
+++ lab02/main.py
@@ -1,7 +1,8 @@
+#!/bin/python3
+
 import sympy as sp
 import graphics as gr
-from typing import List, Tuple
-from math import exp
+from typing import List, Tuple, Set
 
 # Потрібно побудувати графічно фазові траєкторії наступних задач:
 # 1. є d2y(x)/dx2 + (1+x^2)dy(x)/dx - (x - 1/2)^2 y(x) = -(x^2+e^x)
@@ -14,20 +15,21 @@ from math import exp
 #    x in (-1, 1), y(-1) = -1, y(1) = 2, є = 1, 10, 0.00001, 0.1
 
 if __name__ == '__main__':
-
     eps: List[float] = [1.0, 10.0, 0.00001, 0.1]
-    x, y, q = sp.symbols('x y q')  # for reason instead of є there'll be q
-    dy = sp.diff(y, x)
-    ddy = sp.diff(dy, x)
-    problems: List[Tuple[str, Tuple[float, float], Tuple[float, float]]] = [
-        ('q*diff(diff(y, x), x) + (1 + x**2) * diff(y, x) - (x - 0.5)**2 * y = -(x**2 + exp(x))',
-                                                                                             (0.0, 1.0),
-                                                                                             (-1.0, 0.0)),
-        ('q*diff(diff(y, x), x) + diff(y, x) = 0', (0.0, 1.0), (0.0, 1.0)),
-        ('-q*diff(diff(y, x), x) + diff(y, x) = 0', (0.0, 1.0), (1.0, 1.0)),
-        ('q*diff(diff(y, x), x) + 2*x*diff(y, x) = 0', (-1.0, 1.0), (-1.0, 2.0)),
-    ]
 
-    print((problems[0][0]))
+    x = sp.Symbol('x')
+    f = sp.Function('f')
+    df = sp.diff(f, x)
+    ddf = sp.diff(df, x)
 
-    gr.draw_phase_trajectory()
+    eq = sp.dsolve(sp.Eq(ddf(x) + (1 + x**2)*df(x) - (x - 0.5)**2 * f(x), -x**2 - sp.exp(x)), f, ics={f(0): -1, f(1): 0})
+    p = sp.plot(eq, show=False)
+    p.show()
+
+#    plots: List = [sp.plot(problem, (x, 0.0, 1.0), show=False) for problem in problems]
+
+    # plots[0].extend(plots[1])
+    # plots[0].extend(plots[2])
+    # plots[0].extend(plots[3])
+
+#    plots[0].show()
