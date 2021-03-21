ed33500baad8fb206d323bb19164a7828f040f52
AlexValder <anna.loban@yahoo.com>
2021-03-21 15:19:44 +0200

    Lab02 done

--- lab02/graphics.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from typing import List
-
-
-def draw_phase_trajectory(xs: List[float], ys: List[float]) -> None:
-    plt.xlabel('θ')
-    plt.ylabel('dθ/dx')
-    plt.title('Phase space trajectory')
-    plt.show()
